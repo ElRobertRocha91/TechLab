131641096afd1756774816d4f99b520bdcd12d48
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-09-22 20:32:21 -0300

    Actualizando clase menu

--- src/main/java/com/techlab/dominio/Menu.java
+++ src/main/java/com/techlab/dominio/Menu.java
@@ -12,11 +12,11 @@ public class Menu {
         do {
             System.out.println();
             System.out.println("=============================================");
-            System.out.println("---- SISTEMA DE GESTIÓN - TECHLAB ----");
+            System.out.println("------- SISTEMA DE GESTIÓN - TECHLAB --------");
             System.out.println("=============================================");
             System.out.println("1) Agregar producto");
             System.out.println("2) Listar productos");
-            System.out.println("3) Buscar/Actualizar producto");
+            System.out.println("3) Buscar producto");
             System.out.println("4) Eliminar producto");
             System.out.println("5) Crear pedido");
             System.out.println("6) Listar pedidos");
