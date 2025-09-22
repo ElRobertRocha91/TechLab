f5399f6da8cf721d8c8ca5751b0da4abf74ee927
ElRobertRocha91 <robertoestebanrocha91@gmail.com>
2025-09-22 00:58:39 -0300

    Actualizando clase pedido con la clase itempedido

--- src/main/java/com/techlab/pedidos/Pedido.java
+++ src/main/java/com/techlab/pedidos/Pedido.java
@@ -9,25 +9,31 @@ public class Pedido {
     // Variables
     private int id;
     private Cliente cliente;
-    private ArrayList<Producto> productos;
+    // private ArrayList<Producto> productos;
+    private ArrayList<ItemPedido> items;
     private static int contadorPedidos = 1;
 
     // Constructor
     public Pedido(Cliente cliente) {
         this.id = contadorPedidos++;
         this.cliente = cliente;
-        this.productos = new ArrayList<>();
+        // this.productos = new ArrayList<>();
+        this.items = new ArrayList<>();
     }
 
     // Metodo
-    public void agregarProducto(Producto producto) {
-        productos.add(producto);
+    public void agregarProducto(Producto producto, int cantidad) {
+        // productos.add(producto);
+        items.add(new ItemPedido(producto, cantidad));
     }
 
     public double calcularTotal() {
         double total = 0;
-        for (Producto p: productos) {
-            total += p.getPrecio();
+        // for (Producto p: productos) {
+        //    total += p.getPrecio() * p.getCantidadStock();
+        // }
+        for (ItemPedido item: items) {
+            total += item.calcularSubtotal();
         }
         return total;
     }
